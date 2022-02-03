7d749831e84552964a1b7d80db205b56653f9e71
NoodMan <camara.adeline@yahoo.fr>
2022-02-03 21:12:58 +0100

    correction bug

--- src/Vues/Cat/all.php
+++ src/Vues/Cat/all.php
@@ -8,56 +8,56 @@ if (empty($aCats)) : ?>
 <?php
 endif;
 ?>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script> <!-- pour le popover -->
 
 <style>
-    .modal-title{
-        color:black;
-    }
-    #title{
+    .modal-title {
         color: black;
     }
 
+    #text-center {
+        text-align: center;
+    }
+
+    #title {
+        color: black;
+    }
 </style>
 <div class="container">
     <!-- pour center le bloc-->
     <div class="text-center"><i class="fas fa-coffee"></i>
-        BAR : <?= $bar1->getSign(); ?>
+        SHOP : <?= $bar1->getSign(); ?>
     </div>
 
     <table class="table table-dark table-hover">
-        <!-- pour avec le-->
+       
         <thead>
             <!-- pour mettre une barre entre -->
-            <th class="text-center">Nom:</th>
-
-
-
-            <!-- popover -->
-            <span>
-                <th class="text-center">Description:</th>
-                <class="d-inline-block" tabindex="0" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-content="Disabled popover">
-                    <button class="btn btn-primary" type="button" disabled>Disabled button</button>
-            </span>
-            <!-- popover -->
-
-
-            <th class="text-center">N° puce:</th>
+            <th class="p-3 mb-2 bg-secondary bg-gradient text-white" id="text-center">NAME:</th>
+            <th id="text-center" class="p-3 mb-2 bg-secondary bg-gradient text-white">DESCRIPTION:</th>
+            <th class="p-3 mb-2 bg-secondary bg-gradient text-white" id="text-center">CHIP NUMBER:
+            </th>
             <?php if ($_SESSION["Type"] === "manager") : ?>
-                <th class="text-center"> Action </th>
+                <th class="p-3 mb-2 bg-secondary bg-gradient text-white" id="text-center">ACTION: </th>
         </thead>
     <?php endif;
 
             foreach ($aCats as $cat) : ?>
         <tr>
-            <td> <?= $cat->getName() ?> </td>
-            <td type="submit"> <?= $cat->getDescription() ?> </td>
+            <td class="text-center"> <?= $cat->getName() ?> </td>
+            <span>
+                <td class="text-center">
+                    <!-- popover -->
+                    <class="d-inline-block" tabindex="0" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-content="<?= $cat->getDescription() ?>"><?= $cat->getDescription() ?>
+                        <button class="btn btn-dark" type="button" disabled>More...</button>
+                </td>
+            </span>
             <td type="submit" class="text-center"> <?= $cat->getChipNumber() ?> </td>
             <?php if ($_SESSION["Type"] === "manager") : ?>
                 <td>
 
                     <div class="row">
-
+                        <!-- le modal  -->
                         <div class="col btn btn-info me-md-2 edit" data-id="<?= $cat->getId(); ?>" data-bs-toggle="modal" data-bs-target="#exampleModal">
                             <!-- ne fonctionne pas avec : voir memo mac "Uncaught TypeError: this._element is undefined" -->
                             <i class="far fa-edit"></i>
@@ -66,7 +66,11 @@ endif;
                             <div class="modal-dialog">
                                 <div class="modal-content">
                                     <div class="modal-header" class="text-center">
-                                        <h2 class="modal-title" id="exampleModalLabel" > <i class="fas fa-cat"></i>Modify cat...</h2>
+                                        <!-- pour faire le point qui clignotte -->
+                                        <div class="spinner-grow text-dark" role="status">
+                                            <span class="visually-hidden">Loading...</span>
+                                        </div>
+                                        <h2 class="modal-title" id="exampleModalLabel"> <i class="fas fa-cat"></i>Modify cat...</h2>
                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                     </div>
 
@@ -75,9 +79,9 @@ endif;
                                     <div class="modal-body">
                                         <form action='<?= "/cat/$id" ?>' method="POST" id="form_controller">
 
-                                        <h2 id="title" class="outline-dark text-center"> <?= $cat->getName(); ?></h2>
-            &nbsp;
-            &nbsp;
+                                            <h2 id="title" class="outline-dark text-center"> <?= $cat->getName(); ?></h2>
+                                            &nbsp;
+                                            &nbsp;
 
                                             <label id="title" for="name">NAME: </label>
                                             <div class="input-group mb-4">
@@ -116,12 +120,14 @@ endif;
                                     </div>
 
                                     <div class="modal-footer">
-                                        <button type="button" class="btn btn-outline-secondary border border-danger" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-outline-dark border border-danger" data-bs-dismiss="modal">Close</button>
                                         <button type="button" class="btn btn-outline-dark border border-danger">Save</button>
                                     </div>
                                 </div>
                             </div>
                         </div>
+
+                        <!-- <div class="col btn btn-info me-md-2 edit" data-id="<?= $cat->getId(); ?>" data-bs-toggle="modal"data-bs-target="#exampleModal"><i class="far fa-edit"></i> </div> -->
                         <div class="col btn btn-danger me-md-2 delete" data-id="<?= $cat->getId(); ?>" data-name="<?= $cat->getName(); ?>">
                             <i class="far fa-trash-alt"></i>
                         </div>
@@ -142,9 +148,14 @@ endif;
     $(".delete").on("click", function(e) {
 
         if (confirm("Êtes vous sur de vouloir supprimer " + $(this).attr("data-name")) == true) {
-            // window.location.href = "/removecat/" + $(this).attr("data-id");
+            window.location.href = "/removecat/" + $(this).attr("data-id");
         }
     });
+    // pour le popover
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
 </script>
 
 <?php include __DIR__ . "/../Heading/footer.php";
